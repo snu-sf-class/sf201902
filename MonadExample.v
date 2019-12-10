86f393090cffd607402deb0bd41674240559c654
Juneyoung Lee <aqjune@gmail.com>
2019-12-10 17:20:27 +0900

    Update MonadExample.v

--- MonadExample.v
+++ MonadExample.v
@@ -52,3 +52,129 @@ Fixpoint For (b n: nat) (body: nat -> StateM unit) : StateM unit :=
   match n with
   | 0 => ret tt
   | S n' => body b;;
+ 
+
+Definition Read (name: string) : StateM nat :=
+  (fun s => (s name, s)).
+
+Definition Write (x: string) (v: nat) : StateM unit :=
+  (fun s => (tt, updateS s x v)).
+
+Definition summation n : StateM nat :=
+  Write "sum" 0;;
+  For 0 (n+1) (fun i =>
+    sum <- Read "sum";;
+    Write "sum" (sum + i)
+  );;
+  sum <- Read "sum";;
+  ret sum
+.
+
+Compute (summation 100 : StateM nat).
+
+Compute (runStateM (summation 100)).
+
+(****
+  IO Monad
+ ****)
+
+Inductive IOM (X: Type) : Type :=
+| io_ret (x: X) : IOM X
+| io_in (cont: nat -> IOM X) : IOM X
+| io_out (n: nat) (cont: IOM X) : IOM X
+.
+Arguments io_ret {X}.
+Arguments io_in {X}.
+Arguments io_out {X}.
+
+Fixpoint iobind {X Y} (m: IOM X) (f: X -> IOM Y) :=
+  match m with
+  | io_ret x => f x
+  | io_in cont => io_in (fun n => iobind (cont n) f)
+  | io_out n cont => io_out n (iobind cont f)
+  end.
+
+Instance IOMonad : Monad IOM :=
+  {
+    ret X x := io_ret x;
+    bind X Y (m: IOM X) (f: X -> IOM Y) := iobind m f;
+  }.
+
+Fixpoint runIOM (m: IOM unit) (inputs: list nat) : list nat :=
+  match m with
+  | io_ret _ => []
+  | io_in cont =>
+    match inputs with
+    | [] => []
+    | n::inputs' => runIOM (cont n) inputs'
+    end
+  | io_out n cont =>
+    n :: runIOM cont inputs
+  end.
+
+Definition Input : IOM nat :=
+  io_in (fun n => io_ret n).
+
+Definition Output (n: nat) : IOM unit :=
+  io_out n (io_ret tt).
+
+Fixpoint sum (n: nat) : nat :=
+  match n with
+  | 0 => 0
+  | S n' => n + sum n'
+  end.
+
+Definition sumIO : IOM unit :=
+  n <- Input;;
+  Output (sum n);;
+  m <- Input;;
+  Output (sum (n+m)).
+
+Compute runIOM sumIO [10; 10].
+
+(****
+  State Monad Transformer
+ ****)
+
+Definition StateMT (M: Type -> Type) X := State -> M (X * State)%type.
+
+Instance StateMonadT {M} `{Monad M} : Monad (StateMT M) :=
+  {
+    ret X x :=
+      (fun s => ret (x, s));
+    bind X Y (m: StateMT M X) (f: X -> StateMT M Y) :=
+      (fun s =>
+         xs' <- m s;;
+         f (fst xs') (snd xs'));
+  }.
+
+Definition runStateMT {M} `{Monad M} {X} (m: StateMT M X) : M X :=
+  xs <- m initS;; ret (fst xs).
+
+Definition LiftT {M} `{Monad M} {X} : M X -> StateMT M X :=
+  (fun m s => x <- m;; ret (x, s)).
+
+Fixpoint ForT {M} `{Monad M} (b n: nat) (body: nat -> StateMT M unit) : StateMT M unit :=
+  match n with
+  | 0 => ret tt
+  | S n' => body b;;
+            ForT (1+b) n' body
+  end.
+
+Definition ReadT {M} `{Monad M} (name: string) : StateMT M nat :=
+  (fun s => ret (s name, s)).
+
+Definition WriteT {M} `{Monad M} (x: string) (v: nat) : StateMT M unit :=
+  (fun s => ret (tt, updateS s x v)).
+
+Definition sumIOSt : StateMT IOM unit :=
+  n <- LiftT Input;;
+  WriteT "sum" 0;;
+  ForT 0 (n+1) (fun i =>
+    sum <- ReadT "sum";;
+    WriteT "sum" (sum + i)
+  );;
+  sum <- ReadT "sum";;
+  LiftT (Output sum).
+
+Compute (runIOM (runStateMT sumIOSt) [100]).
